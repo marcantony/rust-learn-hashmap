c26d4397a45003e97199eb2e8c4f605c07181ab5
Josh <5685731+marcantony@users.noreply.github.com>
2022-07-05 22:18:27 -0400

    Remove autogenerated test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
 pub mod linked_list;
 pub mod persistent_list;
 pub mod hashmap;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
